638a2f403cfa04d9a9410283e10e59b639f658ad
marco rojouan <marcoprojouan@outlook.fr>
2024-10-29 13:07:06 +0100

    testing things

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,25 @@ import type { ProductType } from './lib/definitons'
 import { useState } from 'react'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import ProductDetails from './components/ProductDetails'
+
 
 
 function App() {
 
   const productList: ProductType[] = data;
   const [productFilter, setProductFilter] = useState(productList)
+  const [detailsMenu, setDetailsMenu] = useState(productList.filter((item) => item.description)
 
 
   return (
     <main className='bg-slate-300'>
       <NavBar />
       <ProductFilter productList={productList} productFilter={productFilter} setProductFilter={setProductFilter} />
-      <ProductBox productList={productFilter} />
+      <article>
+        <ProductBox productList={productFilter} />
+        <ProductDetails />
+      </article>
       <Footer />
     </main>
   )

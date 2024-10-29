518bc2ed9e5897ef6bccedc3ed09203ef77d92d7
marco rojouan <marcoprojouan@outlook.fr>
2024-10-29 12:28:05 +0100

    feat 0.9

---
+++ src/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+
+
+    return (
+        <section className="bg-slate-100 flex items-center justify-between">
+            <img className="w-24" src="https://img.freepik.com/premium-vector/colorful-logo-company-logo_1301270-2358.jpg?semt=ais_hybrid" alt="logo de la marque" />
+            <h1 className="text-3xl">Pin's Mania</h1>
+            <p className="mr-4">by Retro</p>
+        </section>
+    )
+}

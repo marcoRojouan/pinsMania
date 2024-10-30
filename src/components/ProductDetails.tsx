999637d331d7201ab640a92d9d7da9524fff64dc
marco rojouan <marcoprojouan@outlook.fr>
2024-10-30 09:57:17 +0100

    1.1

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -2,8 +2,8 @@ export default function ProductDetails({ currentDetail }: { currentDetail: strin
 
 
     return (
-        <section className="rounded-md min-h-80 mx-8 px-8 max-w-64 px-24 bg-slate-200 flex flex-col items-center gap-8">
-            <h2 className="text-2xl">Description :</h2>
+        <section className="rounded-md min-h-80 mx-12 px-8 max-w-64 px-24 bg-slate-200 flex flex-col items-center gap-8">
+            <h2 className="text-2xl  mx-8 px-8">Description</h2>
             <p className="text-xl">{currentDetail}</p>
         </section>
     )
